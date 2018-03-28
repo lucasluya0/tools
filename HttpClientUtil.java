b3973f54e476182ed679a6d0e12bb53b0c89910e
unknown <18205273313@163.com>
2018-03-28 11:50:23 +0800

    lucas add HttpClientUtil.java

---
+++ HttpClientUtil.java
@@ -0,0 +1,122 @@
+package com.https;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+/*
+ * 利用HttpClient进行post请求的工media paly具类
+ */
+public class HttpClientUtil {//http://192.168.89.141:8080/myMongo3/apiAssociationAction_queryAllEnterprise.do?associationId=%E5%8D%8F%E4%BC%9Aid&enterpriseId=%E4%BC%81%E4%B8%9Aid&enterpriseName=%E4%BC%81%E4%B8%9A%E5%90%8D%E7%A7%B0
+	public static void main(String[] args) {
+		String httpOrgCreateTest = "http://192.168.89.141:8014/myMongo3/apiAssociationAction_queryAllEnterprise.do";  
+		String per = "associationId=协会id&enterpriseId=企业id&enterpriseName=企业名称";
+        Map<String,String> createMap = new HashMap<String,String>();  
+        createMap.put("enterpriseId","****1111***");  
+        createMap.put("enterpriseId","****2222*");  
+        createMap.put("enterpriseName","**3333**");
+        
+        String REP = HttpClientUtil.doPost(httpOrgCreateTest,createMap,"utf-8");  
+        System.out.println("result:"+REP); 
+        
+		
+		String REG = HttpClientUtil.sendGet(httpOrgCreateTest,per);
+		System.out.println(REG);
+	}
+	 /**
+     * 向指定URL发送GET方法的请求
+     * @param url 发送请求的URL
+     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @return URL 所代表远程资源的响应结果
+     */
+    public static String sendGet(String url, String param) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString = url + "?" + param;
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+	
+	public static String doPost(String url,Map<String,String> map,String charset){
+		HttpClient httpClient = null;
+		HttpPost httpPost = null;
+		String result = null;
+		try{
+			httpClient = new SSLClient();
+			httpPost = new HttpPost(url);
+			//设置参数
+			List<NameValuePair> list = new ArrayList<NameValuePair>();
+			Iterator iterator = map.entrySet().iterator();
+			while(iterator.hasNext()){
+				Entry<String,String> elem = (Entry<String, String>) iterator.next();
+				list.add(new BasicNameValuePair(elem.getKey(),elem.getValue()));
+			}
+			if(list.size() > 0){
+				UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list,charset);
+				httpPost.setEntity(entity);
+			}
+			HttpResponse response = httpClient.execute(httpPost);
+			if(response != null){
+				HttpEntity resEntity = response.getEntity();
+				if(resEntity != null){
+					result = EntityUtils.toString(resEntity,charset);
+				}
+			}
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		return result;
+	}
+	
+
+}
